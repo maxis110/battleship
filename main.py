68d1573ab94e3ca76c3f60902b7651aa152f0c41
maksim <mikhailenkomaxim@gmail.com>
2016-08-27 10:29:21 -0700

    Exception added

--- main.py
+++ main.py
@@ -9,25 +9,28 @@ def startGame(counts):
     ship_row = ship[0]
     ship_col = ship[1]
     while counts:
-        shoot = []
-        row = int(raw_input("Select row:"))
-        shoot.append(row)
-        col = int(raw_input("Select colunm:"))
-        shoot.append(col)
-        counts -= 1
-        if shoot not in attempts:
-            attempts.append(shoot)
-            if row == ship_row and col == ship_col:
-                print('Ship founded:',ship_row, ship_col)
-                break
+        try:
+            shot = []
+            row = int(raw_input("Select row:"))
+            shot.append(row)
+            col = int(raw_input("Select colunm:"))
+            shot.append(col)
+            counts -= 1
+            if shot not in attempts:
+                attempts.append(shot)
+                if row == ship_row and col == ship_col:
+                    print('Ship founded:',ship_row, ship_col)
+                    break
+                else:
+                    field[row][col] = 'X'
+                    for i in range(len(field)):
+                        for j in range(len(field[i])):
+                            print (field[i][j],end = ' ')
+                        print ()
             else:
-                field[row][col] = 'X'
-                for i in range(len(field)):
-                    for j in range(len(field[i])):
-                        print (field[i][j],end = ' ')
-                    print ()
-        else:
-            print('Coordinate in use')
+                print('bomb already there')
+        except:
+            print('shot outside the play field')
 
 def createShip():
     ship_row =  random.randrange(0, 2, 1)
@@ -42,6 +45,5 @@ def genField():
         for j in range(0,3):
             field[elem].append(0)
     return field
-
 
 startGame(8)
