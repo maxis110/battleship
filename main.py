b10b1686ed656f24952f770188da54e063325840
maksim <mikhailenkomaxim@gmail.com>
2016-08-28 04:32:48 -0700

    refactoring

--- main.py
+++ main.py
@@ -1,56 +1,53 @@
-
-from __future__ import print_function
 import random
 
-def startGame(counts, ship_quant, field_row, field_col):
+def startGame(ship_quant, size):
+    counts = 0
     attempts = []
-    field = genField(field_row,field_col)
-    ship = createShip(ship_quant)
-    ship_row = ship[0][0]
-    ship_col = ship[0][1]
-    while counts:
-        try:
+    field = genField(size)
+    ship = createShip(ship_quant, field)
+    while ship:
+        if counts < (size*size - ship_quant) :
             shot = []
             row = int(raw_input("Select row:"))
             shot.append(row)
             col = int(raw_input("Select colunm:"))
             shot.append(col)
-            counts -= 1
-            if shot not in attempts:
-                attempts.append(shot)
-                if row == ship_row and col == ship_col:
-                    print('Ship founded:',ship_row, ship_col)
-                    break
+            if col < len(field) and row < len(field):
+                if shot not in attempts:
+                    attempts.append(shot)
+                    if shot in ship:
+                        ship.remove(shot)
+                        print ship
+                        print('Ship founded:',shot)
+                    else:
+                        counts += 1
+                        field[row][col] = 'X'
+                        for i in range(len(field)):
+                            print ' '.join(str(rows) for rows in field[i])
                 else:
-                    field[row][col] = 'X'
-                    for i in range(len(field)):
-                        for j in range(len(field[i])):
-                            print (field[i][j],end = ' ')
-                        print ()
+                    print('bomb already there')
             else:
-                print('bomb already there')
-        except:
-            print('shot outside the play field')
-
-def createShip(quantity):
+                print('out of playboard')
+        else:
+            print('game over')
+            break
+def createShip(quantity, field):
     fleet = []
-    for elem in range(quantity):
+    while quantity:
         ship = []
-        ship_row = random.randrange(0, 2, 1)
+        ship_row = random.randrange(0, len(field), 1)
         ship.append(ship_row)
-        ship_column = random.randrange(0, 2, 1)
+        ship_column = random.randrange(0, len(field), 1)
         ship.append(ship_column)
         if ship not in fleet:
             fleet.append(ship)
-    print(fleet)
+            quantity -= 1
+    print ('random ship on: ',fleet)
     return fleet
 
-def genField(rows, columns):
-    field = []
-    for elem in range(rows):
-        field.append([])
-        for j in range(columns):
-            field[elem].append(0)
-    return (field)
+def genField(size):
+    field=[]
+    [field.append(['0']*size) for elem in range(size)]
+    return field
 
-startGame(8,1,3,3)
+startGame(6,3)
